2c31f4287868b115edd9f3c91464eeb3eb50a5d2
Lucas Casarin Marquezini <lcasarin1@hotmail.com>
2017-10-06 20:51:11 -0300

    Tudo Funcionando

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+//Arquivo para usar nos projetos
+// É necessario usar outro arquivo para importar as pastas css, fonts, js para o projeto
+// const gulp        = require('gulp');
+// const browserSync = require('browser-sync').create();
+// const sass        = require('gulp-sass');
+//
+// // Compile Sass & Inject Into Browser
+// gulp.task('sass', function() {
+//     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+//         .pipe(sass())
+//         .pipe(gulp.dest("src/css"))
+//         .pipe(browserSync.stream());
+// });
+// // Watch Sass & Serve
+// gulp.task('serve', ['sass'], function() {
+//
+//     browserSync.init({
+//         server: "./src"
+//     });
+//
+//     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+//     gulp.watch("src/*.html").on('change', browserSync.reload);
+// });
+// gulp.task('default', ['serve']);
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+// Move JS Files to src/js
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+});
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Move Fonts to src/fonts
+gulp.task('fonts', function() {
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('src/fonts'))
+})
+
+// Move Font Awesome CSS to src/css
+gulp.task('fa', function() {
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest('src/css'))
+})
+
+gulp.task('default', ['js','serve', 'fa', 'fonts']);
